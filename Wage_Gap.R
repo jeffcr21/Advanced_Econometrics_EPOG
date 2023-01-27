e12bf43d5f3c5a8e23ee739bb40bb9e3bd9e4e9a
Jeff Rodriguez <jeff.rodriguez@edu.unito.it>
2023-01-27 18:22:58 +0100

    Fixed the models and added some result tables

--- Wage_Gap.R
+++ Wage_Gap.R
@@ -64,17 +64,18 @@ cpsgen_no <- cpsgen_no %>% mutate(hs = ifelse(sch >= 12, 1, 0))
 cpsgen_no <- cpsgen_no %>% mutate(age_squared = age^2)
 
 
+
 # Calculating the wage gap ------------------------------------------------
 
 ## Now we can calculate the wage gap in Illinois in 2000 and 2006 (This is just for descriptive purposes)
 
 ## First, I will recode the sex variable
 
-cpsgen_no <- cpsgen_no %>% mutate(sex = (recode(sex, '2' = '0', '1' = '1')))
+cpsgen_no <- cpsgen_no %>% mutate(fem = (recode(sex, '2' = '1', '1' = '0')))
 
 wagegap <- function(data, x, y, z) {
   wagegap <- data %>% {{filter}}(
-    sex == x &
+    fem == x &
       year == y &
       statefip == z) %>% 
     {{summarise}}(mean(realhrwage, na.rm = TRUE)
@@ -90,8 +91,29 @@ wagegap(cpsgen_no, 1, 2006, 17)
 wagegap(cpsgen_no, 0, 2000, 17) - wagegap(cpsgen_no, 1, 2000, 17)
 wagegap(cpsgen_no, 0, 2006, 17) - wagegap(cpsgen_no, 1, 2006, 17)
 
-wagegap(cpsgen_no, 0, 2000, !17) - wagegap(cpsgen_no, 1, 2000, !17)
-wagegap(cpsgen_no, 0, 2006, !17) - wagegap(cpsgen_no, 1, 2006, !17)
+## Wagegap 2000 = 8.82 USD; 2006 = 3.88 USD
+
+## We can also fit a regression:
+
+gap_illi_2000 <- cpsgen_no %>% filter(
+  year == 2000 &
+    statefip == 17
+)
+
+gap_illi_2000_m1 <- lm(realhrwage ~ fem, data = gap_illi_2000)
+
+summary(gap_illi_2000_m1)
+
+gap_illi_2006 <- cpsgen_no %>% filter(
+  year == 2006 &
+    statefip == 17
+)
+
+gap_illi_2006_m1 <- lm(realhrwage ~ fem, data = gap_illi_2006)
+
+stargazer(gap_illi_2000_m1, gap_illi_2006_m1, type = "text")
+
+## Not surprisingly, the regression gives the same results!
 
 ## Now we can start preparing the necessary conditions for a fist exploratory model
 
@@ -103,33 +125,59 @@ cpsgen_no <- cpsgen_no %>% mutate(id = paste(serial, statefip, pernum, sep = "")
 
 cpsgen_no <- cpsgen_no %>% mutate(treated = ifelse(statefip == 17, 1, 0))
 
-#After. The implementation of the law was in 2003
+#Post: The implementation of the law was in 2003
 
-cpsgen_no <- cpsgen_no %>% mutate(after = ifelse(year > 2003, 1, 0))
+cpsgen_no <- cpsgen_no %>% mutate(post = ifelse(year > 2003, 1, 0))
 
 ##Treated + post
 
-cpsgen_no <- cpsgen_no %>% mutate(post.treated = ifelse(treated == 1 & after == 1, 1, 0))
+cpsgen_no <- cpsgen_no %>% mutate(post.treated = ifelse(treated == 1 & post == 1, 1, 0))
 
-##Treated + post + women
+##Post + female
 
-cpsgen_no <- cpsgen_no %>% mutate(post.treated.fem = ifelse(treated == 1 & after == 1 & sex == 0, 1, 0))
+cpsgen_no <- cpsgen_no %>% mutate(post.fem = ifelse(post == 1 & fem == 1, 1, 0))
 
-## Now, to start we can compare Illinois to Indiana, which does NOT have any policy on pay transparency. They are
+## Treated + female
+
+cpsgen_no <- cpsgen_no %>% mutate(treated.fem = ifelse(treated == 1 & fem == 1, 1, 0))
+
+##Treated + post + female
+
+cpsgen_no <- cpsgen_no %>% mutate(post.treated.fem = ifelse(treated == 1 & post == 1 & fem == 1, 1, 0))
+
+## Now, to start we can compare Illinois to it's neighboring states (Wisconsin, Indiana, 
+## Missoury and Iowa), which does NOT have any policy on pay transparency. They are
 ## neighboring states and therefore might share some demographic characteristics. In any case, we can check that.
 
 II <- cpsgen_no %>% filter(
-  statefip == 17 | statefip == 18
+  statefip == 17 | statefip == 18 | statefip == 19 | statefip == 29 | statefip == 55
 )
 
-boxplot(II$age, II$realhrwage, II$lnrwg)
+## We can test some characteristics of the data:
 
-model1 <- lm(lnrwg ~ treated + after + sex + post.treated + post.treated.fem + age + race + hrswork + ba + adv + ft,  data = II)
+t.test(II$age[II$treated == 1], II$age[II$treated == 0])
+
+ggplot(data = II, aes(x = realhrwage, y = ..density..)) +
+  geom_density(aes(color = as.factor(treated), fill = as.factor(treated)),
+               position = "identity", bins = 30, alpha = 0.4)
+
+ggplot(data = II, aes(x = age, y = ..density..)) +
+  geom_density(aes(color = as.factor(treated), fill = as.factor(treated)),
+               position = "identity", bins = 30, alpha = 0.4)
+
+ggplot(data = II, aes(x = hrswork, y = ..density..)) +
+  geom_density(aes(color = as.factor(treated), fill = as.factor(treated)),
+               position = "identity", bins = 30, alpha = 0.4)
+
+## And then try a first model comparing Illinois and Indiana
+
+model1 <- lm(lnrwg ~ treated + post + fem + post.treated + treated.fem + post.fem + post.treated.fem,  data = II)
 
 summary(model1)
 
-## Question: Should I use lm or plm? **
+model2 <- lm(lnrwg ~ treated + post + fem + post.treated + treated.fem + post.fem + post.treated.fem + age + race + hrswork + ba + adv + ft,  data = II)
 
+summary(model2)
 
 # Matching Method ---------------------------------------------------------
 
@@ -148,9 +196,9 @@ ggplot(data = cpsgen_no, aes(x = realhrwage, y = ..density..)) +
 data_nomiss <- cpsgen_no %>% 
   na.omit()
 
-data_nomiss$sex <- as.numeric(data_nomiss$sex)
+data_nomiss$fem <- as.numeric(data_nomiss$fem)
 
-m1 <- matchit(treated ~ year + age + sex + race + marst + sch + classwkr + union, method = "nearest", data = data_nomiss)
+m1 <- matchit(treated ~ year + age + fem + race + marst + sch + classwkr, method = "nearest", data = data_nomiss)
 summary(m1)
 # plot(m1, type = "jitter")
 m1data <- match.data(m1)
@@ -171,6 +219,12 @@ wagegap2(m1data, 1, 1, 1)
 
 #t.test(m1data$lnrwg[m1data$treated == 1], m1data$lnrwg[m1data$treated == 0], paired = T)
 
-model_2 <- lm(lnrwg ~  treated + after + sex + post.treated + post.treated.fem, data = m1data)
-model_2
-summary(model_2)
+model3 <- lm(lnrwg ~ treated + post + fem + post.treated + treated.fem + post.fem + post.treated.fem,  data = m1data)
+
+summary(model3)
+
+model4 <- lm(lnrwg ~ treated + post + fem + post.treated + treated.fem + post.fem + post.treated.fem + age + race + hrswork + ba + adv + ft,  data = m1data)
+
+summary(model4)
+
+stargazer(model1, model2, model3, model4, type = "text")
